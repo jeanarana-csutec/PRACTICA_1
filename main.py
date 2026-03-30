45817b3c724c844bde1c163cd1069ff50ac0bff5
Santiago <santiago.morales@utec.edu.pe>
2026-03-30 01:46:28 -0500

    Prueba error

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+
 import os
 
-print("Bienvenido al juego del laberinto")
+print("Bienvenido al juego del laberinto, esto lo hize yooo jajajaja")
+print("Hola cachimbos webones")
 
 while True:
     opcion = input("Escribe 'jugar' para empezar o 'salir' para terminar: ").strip().lower()
